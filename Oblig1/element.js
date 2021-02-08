a2294ca715e4998606c8c155ea2edb5dfc35ed32
Glenn Hansen <hansenglenneirik@gmail.com>
2021-02-09 00:19:48 +0100

    Finished Oblig

--- Oblig1/element.js
+++ Oblig1/element.js
@@ -1,24 +1,56 @@
 class Coffee extends HTMLElement {
     constructor() {
         super();
-        this.level = this.getAttribute("level") || "Not measured";
-        this.prepared = this.getAttribute("prepared") || "Not measured";
-        this.temperature = this.getAttribute("temperature") || "Not measured";
-
         this.attachShadow({ mode: "open" });
+
+        this._level;
+        this._preparedAt;
+        this._temperature;
+    }
+
+    getLevel(){
+        if(this._level === 0){
+            return 'Empty';
+        }else if(this._level <= 0.5){
+            return 'Almost empty';
+        }else if(this._level <= 1.1){
+            return 'Almost full';
+        }else if(this._level <= 2.2){
+            return 'Full';
+        };
+    }
+
+    getPrepared(){
+        return new Date(this._preparedAt).toUTCString();
+    }
+
+    getTemperature(){
+        if(this._temperature >= 54){
+            return `${this._temperature} C - Hot`;
+        }else{
+            return `${this._temperature} C - Cold`;
+        }
+    }
+
+    setLevel(level){
+        this._level = level;
+        console.log(level);
+    }
+
+    setPreparedAt(preparedAt){
+        this._preparedAt = preparedAt;
+    }
+
+    setTemperature(temperature){
+        this._temperature = temperature;
+    }
+
+    setValues(level, preparedAt, temperature){
+        this.setLevel(level);
+        this.setPreparedAt(preparedAt);
+        this.setTemperature(temperature);
+
         this.shadowRoot.innerHTML = this._getTemplate();
-    }
-
-    getLevel() {
-        return this.level;
-    }
-
-    getPrepared() {
-        return this.prepared;
-    }
-
-    getTemperature() {
-        return this.temperature;
     }
 
     _getTemplate() {
@@ -48,3 +80,17 @@ class Coffee extends HTMLElement {
 }
 
 customElements.define("coffee-element", Coffee);
+
+function loadInfo() {
+    fetch('items.json')
+        .then(response => response.json())
+        .then(data => {
+            console.log(data);
+            let coffee = document.getElementsByTagName("coffee-element");
+            for(let i=0; i<data.length; i++){
+                if(coffee[i]){
+                    coffee[i].setValues(data[i].level, data[i].preparedAt, data[i].temperature);
+                }
+            }
+        });
+}
