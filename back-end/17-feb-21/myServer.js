776930d4d5eb992d1499b54c7763d67dd96c857a
Glenn Hansen <hansenglenneirik@gmail.com>
2021-02-17 13:43:35 +0100

    Added index and form

--- back-end/17-feb-21/myServer.js
+++ back-end/17-feb-21/myServer.js
@@ -1,7 +1,24 @@
 const http = require('http');
 
-http.createServer((req, res) => { // Create server object   
-    res.write('Server here!');    // Send to the client
-    res.end();
+http.createServer((req, res) => { // Create server object 
+    const myUrl = new URL(req.url, "http://localhost:8081");
+    console.log(myUrl);
+
+    res.writeHead(200, { 'User-message': 'The file was not found', 'Content-Type': 'text/html' });
+
+    const backgroundColor = myUrl.searchParams.get('background-color').toString();
+    res.end(`
+        <html>
+            <head>
+                <style>
+                    body {
+                        background-color: ${backgroundColor};
+                    }
+                </style>
+            </head>
+            <body>
+            </body>
+        </html>
+    `);
 }).listen(8081);
 
