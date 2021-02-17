aaf87c6641fb8dc2aecae9efebc354aaed37fa5b
Glenn Hansen <hansenglenneirik@gmail.com>
2021-02-17 11:20:06 +0100

    Added exit process

--- back-end/17-feb-21/questions.js
+++ back-end/17-feb-21/questions.js
@@ -6,7 +6,7 @@ const questions = [
 
 const ask = (i=0) => {
     process.stdout.write(`\n ${questions[i]} \n`);
-    process.stdout.write('->');
+    process.stdout.write('-> ');
 };
 
 ask();
@@ -14,13 +14,22 @@ ask();
 const answers = [];
 
 process.stdin.on("data", (data) => {
-    process.stdout.write(`\n ${data.toString().trim()} \n`);
+    //process.stdout.write(`\n ${data.toString().trim()} \n`);
     answers.push(data.toString().trim());
 
     if(answers.length < questions.length){
         ask(answers.length);
     } else{
-        process.stdout.write(`Hi ${answers[0]}, you can go and ${answers[1]} while having a ${answers[2]}.`);
         process.exit();
     }
+});
+
+process.on("exit", () => {
+    const [name, activity, drink] = answers;
+    console.log(`
+    Thank you for your answers!
+
+    Hi ${name}, you can go and ${activity} while having a ${drink}!
+    Later gator ;)
+    `);
 });
