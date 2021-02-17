04dbe76f0bca91efe377a7056bbaa141764a0209
Glenn Hansen <hansenglenneirik@gmail.com>
2021-02-17 14:09:42 +0100

    finished lecture

--- back-end/17-feb-21/myServer.js
+++ back-end/17-feb-21/myServer.js
@@ -4,9 +4,32 @@ http.createServer((req, res) => { // Create server object
     const myUrl = new URL(req.url, "http://localhost:8081");
     console.log(myUrl);
 
-    res.writeHead(200, { 'User-message': 'The file was not found', 'Content-Type': 'text/html' });
+    res.writeHead(200, {
+        'User-message': 'The file was not found', 'Content-Type': 'text/html', 'firstName': myUrl.searchParams.get('firstName')
+    });
 
-    const backgroundColor = myUrl.searchParams.get('background-color').toString();
+    let backgroundColor;
+    if (myUrl.searchParams.get('favColor')) {
+        backgroundColor = myUrl.searchParams.get('favColor').toString();
+    } else {
+        backgroundColor = 'white';
+    }
+
+    let firstName;
+    if (myUrl.searchParams.get('firstName')) {
+        firstName = myUrl.searchParams.get('firstName').toString();
+    } else {
+        firstName = "stranger";
+    }
+
+    let lastName;
+    if (myUrl.searchParams.get('lastName')) {
+        lastName = myUrl.searchParams.get('lastName').toString();
+    } else {
+        lastName = "";
+    }
+
+
     res.end(`
         <html>
             <head>
@@ -17,6 +40,8 @@ http.createServer((req, res) => { // Create server object
                 </style>
             </head>
             <body>
+                <h1>Hello ${firstName} ${lastName}!</h1>
+                <p>Here is your page color ${backgroundColor}.</p>
             </body>
         </html>
     `);
