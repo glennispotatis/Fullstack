e8c5f63bd9104b586ea710c5f756d5c2f4a4e9b4
Glenn Hansen <hansenglenneirik@gmail.com>
2021-02-19 14:12:40 +0100

    added state examples

---
+++ React/lecture-react2/react-state-example/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import MyButton from './components/Mybutton';
+
+function App() {
+  return (
+    <div className="App">
+      <MyButton />
+    </div>
+  );
+}
+
+export default App;
---
+++ React/lecture-react2/react-state-example/src/components/Mybutton.jsx
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+
+class MyButton extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { nClicks: 0 };
+        this.handleClick = this.handleClick.bind(this);
+    }
+
+    handleClick(){
+        console.log(`"this" value is: ${this}`);
+        this.state.nClicks+=1;
+        console.log(`The number of clicks is: ${this.state.nClicks}`);
+    }
+
+    render() { 
+        return ( 
+            <button onClick={this.handleClick}>Clicked {this.state.nClicks} times</button>
+        );
+    }
+}
+
+export default MyButton;
