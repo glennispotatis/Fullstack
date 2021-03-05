180838e65d4ed4e5598467f36f2cd22a716fe4b4
Glenn Hansen <hansenglenneirik@gmail.com>
2021-03-05 16:06:30 +0100

    Added components from Carlos

--- oblig2/src/App.js
+++ oblig2/src/App.js
@@ -1,26 +1,115 @@
-import './App.css';
+import React, { Component } from 'react';
 import {
   BrowserRouter as Router,
   Switch,
   Route,
-  Link,
-  Redirect,
-  useHistory,
-  useLocation
+  Link
 } from "react-router-dom";
 
-function App() {
-  return (
-    <div className="App">
-      <Router>
-        <Switch>
-          <Route exact path="login">
-            
-          </Route>
-        </Switch>
-      </Router>
-    </div>
-  );
+import './App.css';
+import USERS from './users';
+
+import Home from './components/home/Home';
+import UserList from './components/user-list/user-list';
+import NotFound from './components/NotFound';
+import About from './components/about/About';
+import PrivateRoute from './routes/PrivateRoute';
+import Login from './components/login/Login';
+
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    // posible values "available/busy and on-campus/home-office"
+    this.state = {
+      myUser: { ...USERS[0] },
+      users: [...USERS]
+    }
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Router>
+          <div className="App">
+            <header className="App-header">
+              <nav>
+                <ul>
+                  <li>
+                    <Link to="/">Home</Link>
+                  </li>
+                  <li>
+                    <Link to="/user">User home</Link>
+                  </li>
+                  <li>
+                    <Link to="/dashboard">Dashboard</Link>
+                  </li>
+                  <li>
+                    <Link to="/login">Login</Link>
+                  </li>
+                </ul>
+              </nav>
+            </header>
+            <main>
+              <Switch>
+                <Route path="/dashboard">
+                  <UserList users={this.state.users} />
+                </Route>
+                <PrivateRoute exact path="/user">
+                  <Home user={this.state.myUser} onChangePlace={this.updateUserPlace} onChangeStatus={this.updateUserStatus} />
+                </PrivateRoute>
+                <Route path="/login">
+                  <Login/>
+                </Route>
+                <Route path="/">
+                  <About />
+                </Route>
+                <Route>
+                  <NotFound />
+                </Route>
+              </Switch>
+            </main>
+          </div>
+        </Router>
+      </div>
+    );
+  }
+
+  updateUserPlace = (onCampus) => {
+    const place = onCampus ? 'on-campus' : 'home-office';
+    this.setState((state) => {
+
+      let newUserList = [...this.state.users];
+      //myUser is always in pos[0] in the demo. However, in real app this will not be true.
+      newUserList[0].place = place;
+
+      return {
+        myUser: {
+          ...state.myUser,
+          place
+        },
+        users: newUserList
+      }
+    });
+  }
+
+  updateUserStatus = (available) => {
+    const status = available ? 'available' : 'busy';
+    this.setState((state) => {
+
+      let newUserList = [...this.state.users];
+      //myUser is always in pos[0] in the demo. However, in real app this will not be true.
+      newUserList[0].status = status;
+
+      return {
+        myUser: {
+          ...state.myUser,
+          status
+        },
+        users: newUserList
+      }
+    });
+  }
 }
 
 export default App;
---
+++ oblig2/src/users.js
@@ -0,0 +1,26 @@
+//only for testing purposes
+const busyHome = {
+    name: 'My user',
+    status: 'busy',
+    place: 'home-office'
+}
+
+const busyCampus = {
+    name: 'JaneBuCa',
+    status: 'busy',
+    place: 'on-campus'
+}
+
+const availableHome = {
+    name: 'JaneAvHo',
+    status: 'available',
+    place: 'home-office'
+}
+
+const availableCampus = {
+    name: 'JaneAvCa',
+    status: 'available',
+    place: 'on-campus'
+}
+const users = [busyHome, busyCampus, availableHome, availableCampus];
+export default users;
