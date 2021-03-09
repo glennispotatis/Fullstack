fd71d3d28150d3dea3dc9e02e46dca492470f276
Glenn Hansen <hansenglenneirik@gmail.com>
2021-03-09 12:53:40 +0100

    Added uid for login

--- oblig2/src/components/login/Login.jsx
+++ oblig2/src/components/login/Login.jsx
@@ -3,11 +3,12 @@ import { Redirect, Link } from "react-router-dom";
 import './Login.css';
 import SignUp from '../forms/SignUp';
 import '../forms/forms.css';
+import { UIDConsumer } from 'react-uid';
 
 //This component is implementing more than one functionality only for academic purposes.
 //If the app is connected to a Backend, the auth logic should be implemented in a different file (SOLID)
 function Login(props) {
-    if(props.redirect){
+    if (props.redirect) {
         return <Redirect to={props.redirect} />
     }
 
@@ -15,12 +16,23 @@ function Login(props) {
         <div className="Login">
             {!props.isAuth &&
                 <>
-                    <form action="" method="GET" onSubmit={props.handleLogIn}>
-                        <label htmlFor="email">E-mail:</label>
-                        <input type="email" name="email" />
-
-                        <label htmlFor="password">Password:</label>
-                        <input type="password" name="password" />
+                    <form action="#" method="GET" onSubmit={props.handleLogIn}>
+                        <UIDConsumer name={ id => `login-email-${id}`}>
+                            {id => (
+                                <>
+                                    <label htmlFor={id}>E-mail:</label>
+                                    <input id={id} type="email" name="email" />
+                                </>
+                            )}
+                        </UIDConsumer>
+                        <UIDConsumer name={ id => `login-password-${id}`}>
+                            {id => (
+                                <>
+                                    <label htmlFor={id}>Password:</label>
+                                    <input id={id} type="password" name="password" />
+                                </>
+                            )}
+                        </UIDConsumer>
                         <button type="submit">Log In</button>
                     </form>
                     <Link to="/forgot">Forgot password?</Link>
