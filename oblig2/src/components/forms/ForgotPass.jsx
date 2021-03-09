ca2bcbf496744cc92754953f1cb5c04b2b738255
Glenn Hansen <hansenglenneirik@gmail.com>
2021-03-09 12:55:47 +0100

    Added UID for forgotpass

--- oblig2/src/components/forms/ForgotPass.jsx
+++ oblig2/src/components/forms/ForgotPass.jsx
@@ -1,11 +1,18 @@
 import './forms.css';
+import { UIDConsumer } from 'react-uid';
 
 function ForgotPass(props) {
     return (
         <section className="log-in">
             <form action="" method="GET">
-                <label htmlFor="email">Your user email:</label>
-                <input type="email" name="email" required />
+                <UIDConsumer name={id => `forgot-email-${id}`}>
+                    {id => (
+                        <>
+                            <label htmlFor={id}>Your user email:</label>
+                            <input id={id} type="email" name="email" required />
+                        </>
+                    )}
+                </UIDConsumer>
                 <input type="submit" value="Send" />
             </form>
         </section>
