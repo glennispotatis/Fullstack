7ee088d86e12223e9555da4c3e119f48c29d84ba
Glenn Hansen <hansenglenneirik@gmail.com>
2021-03-12 13:09:58 +0100

    Finished redux

--- react-redux-first-app-activity/src/components/TotalClicks.jsx
+++ react-redux-first-app-activity/src/components/TotalClicks.jsx
@@ -1,7 +1,15 @@
+import { connect } from 'react-redux';
+
 const TotalClicks = (props) => {
     return (
         <p>The total number of clicks during the execution of this app is: <strong>{props.clickCounter}</strong></p>
     );
 }
 
-export default TotalClicks;
+const mapStateToProps = (state) => {
+    return {
+        clickCounter: state.clickCounter
+    }
+}
+
+export default connect(mapStateToProps)(TotalClicks);
